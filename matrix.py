c9e145fb04d3a2b173eface88ad3638577c423fa
gabrysiam8 <gabrysiamiedlar@o2.pl>
2019-03-22 12:50:08 +0100

    matrix init function fixed

--- matrix.py
+++ matrix.py
@@ -5,33 +5,38 @@ class Matrix:
 	def __init__(self, *arg):
 		try:
 			if len(arg)!=4:
-				raise Exception("Bad number of arguments")
+				raise Exception("Bad number of arguments!")
 		except:
-			print("Unexpected error:", sys.exc_info()[0])
-		self._rows=[]
-		for i in range(2):
-			self._rows.append([])
-			for j in range(2):
-				self._rows[i].append(arg[i*2+j])
+			print("Unexpected error:", sys.exc_info()[1])
+		self._rows=[[arg[i*2+j] for j in range(2)] for i in range(2)]
 
 	def __str__(self):
 		return str(self._rows)
 
 	def add(self, other):
-		if isinstance(other, Matrix):
-			new_mat = [[self._rows[i][j]+other._rows[i][j] for j in range(2)] for i in range(2)]
-			return new_mat
-		return None
+		try:
+			if isinstance(other, Matrix):
+				new_mat = [[self._rows[i][j]+other._rows[i][j] for j in range(2)] for i in range(2)]
+				return new_mat
+			else:
+				raise Exception("Bad type of argument!")
+		except:
+			print("Unexpected error:", sys.exc_info()[1])
 
 	def mul(self, other):
-		if isinstance(other, Matrix):
-			new_mat=[]
-			for i in range(2):
-				new_mat.append([])
-				for j in range(2):
-					new_mat[i].append(sum([self._rows[i][k]*other._rows[k][j] for k in range(2)]))
-			return new_mat
-		return None
+		try:
+			if isinstance(other, Matrix):
+				new_mat=[]
+				for i in range(2):
+					new_mat.append([])
+					for j in range(2):
+						new_mat[i].append(sum([self._rows[i][k]*other._rows[k][j] for k in range(2)]))
+				return new_mat
+			else:
+				raise Exception("Bad type of argument!")
+		except:
+			print("Unexpected error:", sys.exc_info()[1])
+		
 
 
 
--- task.py
+++ task.py
@@ -6,7 +6,7 @@ print(mat1)
 mat2 = Matrix(2,3,4,5)
 print(mat2)
 
-mat3=mat1.add(mat2)
+mat3=mat1.add(2)
 print(mat3)
 
 mat4=mat1.mul(mat2)
